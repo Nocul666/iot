5ed13fcc34cb972c15e7cecccb60bed80464a6dc
Nocul <lukasz.noculak@onet.pl>
2021-04-18 12:47:47 +0200

    dsa

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "dowolny-string";
 // List of files which are store in cache.
-let filesToCache = ["/", "styles.css", "Capture1.png", "main.js"];
+let filesToCache = ["/", "styles.css", "Capture1.png", "script.js"];
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches
