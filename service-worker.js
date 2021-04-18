efb56b5551abc03dc96395b4403aafc76fd149bb
Nocul <lukasz.noculak@onet.pl>
2021-04-18 14:21:21 +0200

    m

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,26 @@
 const CACHE_NAME = "dowolny-string";
 // List of files which are store in cache.
 let filesToCache = ["/", "Capture1.png", "script.js"];
+
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then(function (cache) {
+      .then(function(cache) {
         return cache.addAll(filesToCache);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         // Snooze errors...
         // console.error(err);
       })
   );
 });
-self.addEventListener("fetch", function (evt) {
+self.addEventListener("fetch", function(evt) {
   // Snooze logs...
   // console.log(event.request.url);
   evt.respondWith(
     // Firstly, send request..
-    fetch(evt.request).catch(function () {
+    fetch(evt.request).catch(function() {
       // When request failed, return file from cache...
       return caches.match(evt.request);
     })
@@ -32,7 +33,7 @@ function isSuccessful(response) {
     response.type === 'basic';
 }
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function (response) {
@@ -52,8 +53,7 @@ self.addEventListener('fetch', function (event) {
               });
 
             return response;
-          }
-        );
+          });
       })
     );
 });
